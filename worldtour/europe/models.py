e630899158a49dc58b742ff1108e57143520227f
Safa Asgar <safaasgar58@gmail.com>
2023-08-19 02:31:54 +0600

    Created Admin

--- worldtour/europe/models.py
+++ worldtour/europe/models.py
@@ -6,3 +6,6 @@ class Trip(models.Model):
     destination = models.CharField(max_length=64)
     nights = models.IntegerField()
     price = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f"{self.id} - {self.origin} TO {self.destination} for {self.nights}, costs {self.price} EUR"
